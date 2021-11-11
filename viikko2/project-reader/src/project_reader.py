2424ab5653f23facd3e8f5767b346c1166bd5cfa
Viivi Baarman <viivi.baarman@gmail.com>
2021-11-11 12:26:34 +0200

    muutos

--- viikko2/project-reader/src/project_reader.py
+++ viikko2/project-reader/src/project_reader.py
@@ -10,9 +10,6 @@ class ProjectReader:
     def get_project(self):
         # tiedoston merkkijonomuotoinen sisältö
         content = request.urlopen(self._url).read().decode("utf-8")
-        print(content)
-
         _toml = toml.loads(content)
 
-        # deserialisoi TOML-formaatissa oleva merkkijono ja muodosta Project-olio sen tietojen perusteella
         return Project(_toml["tool"]["poetry"]["name"], _toml["tool"]["poetry"]["description"], _toml["tool"]["poetry"]["dependencies"].keys(), _toml["tool"]["poetry"]["dev-dependencies"].keys())
